718f3aea16ea8db5a6499b84a06a45b40b9a75e5
Matheus Toledo <m.toledo@nextage.com.br>
2022-03-11 17:12:42 -0300

    feat: Create home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,22 @@
 import React from 'react'
-import { View, Text, StyleSheet } from 'react-native'
+import { 
+    View, 
+    Text, 
+    StyleSheet, 
+    TextInput, 
+    Platform ,
+    TouchableOpacity,
+} from 'react-native'
 
 export default function Home() {
   return (
     <View style={styles.container}>
-      <Text>My Skills</Text>
+      <Text style={styles.title}>Welcome, Matheus</Text>
+      <TextInput style={styles.input} placeholder="New skill" placeholderTextColor="#555" />
+      <TouchableOpacity style={styles.button} activeOpacity={.7}>
+          <Text style={styles.buttonText}>Add</Text>
+      </TouchableOpacity>
+      <Text style={[ styles.title, { marginTop: 50 } ]}>My Skills</Text>
     </View>
   )
 }
@@ -12,7 +24,33 @@ export default function Home() {
 const styles = StyleSheet.create({
     container: {
         flex: 1, 
-        justifyContent: 'center', 
-        alignItems: 'center',     
+        backgroundColor: '#121015',
+        paddingHorizontal: 30,
+        paddingVertical: 70,
+    },
+    title: {
+        color: '#FFF',
+        fontSize: 24,
+        fontWeight: 'bold',
+    },
+    input: {
+        backgroundColor: '#1F1E25',
+        color: '#FFF',
+        fontSize: 18,
+        padding: Platform.OS === 'ios' ? 15 : 10,
+        marginTop: 30,
+        borderRadius: 7
+    },
+    button: {
+        backgroundColor: '#A370F7',
+        padding: Platform.OS === 'ios' ? 20 : 15,
+        borderRadius: 7,
+        alignItems: 'center',
+        marginTop: 20,
+    },
+    buttonText: {
+        color: "#FFF",
+        fontSize: 17,
+        fontWeight: 'bold',
     },
 })
